c4ad04a3bc85ea22e1d5839e23c1edd2f34bea02
9-SK <89019214+9-SK@users.noreply.github.com>
2023-10-02 11:02:13 +0530

    Fixed error_400.php

--- template/errors/custom/error_400.php
+++ template/errors/custom/error_400.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?= $this->base->text('user_inactive', 'title') ?> - <?= $this->base->get_hostname() ?></title>
+	<title><?php echo $this->base->text('user_inactive', 'title') ?> - <?php echo $this->base->get_hostname() ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="icon" type="image/png" href="<?= base_url()?>assets/<?= $this->base->get_template() ?>/img/fav.png">
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/<?= $this->base->get_template() ?>/css/tabler.min.css">
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/<?= $this->base->get_template() ?>/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>assets/<?= $this->base->get_template() ?>/css/style.css">
+	<link rel="icon" type="image/png" href="<?php echo base_url()?>assets/<?php echo $this->base->get_template() ?>/img/fav.png">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/<?php echo $this->base->get_template() ?>/css/tabler.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/<?php echo $this->base->get_template() ?>/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/<?php echo $this->base->get_template() ?>/css/style.css">
 </head>
-<body class="border-top-wide border-primary d-flex flex-column theme-<?= get_cookie('theme') ?? 'light' ?>">
+<body class="border-top-wide border-primary d-flex flex-column theme-<?php echo get_cookie('theme') ?? 'light' ?>">
 	<div class="page page-center">
 		<div class="container text-center">
 			<div class="empty">
-	        <div class="empty-header"><i class="fa fa-envelope-open"></i></div>
-		        <p class="empty-title"><?= $this->base->text('oops_note', 'paragraph') ?></p>
+		        <div class="empty-header"><i class="fa fa-envelope-open"></i></div>
+		        <p class="empty-title"><?php echo $this->base->text('oops_note', 'paragraph') ?></p>
 		        <p class="empty-subtitle text-muted">
-		        	<?= $this->base->text('user_inactive_note', 'paragraph') ?> <a href="<?= base_url() ?>/e/error_400?resend=true"><?= $this->base->text('resend_email', 'button') ?></a> <?= $this->base->text('or', 'label') ?> <a href="<?= base_url() ?>/e/error_400?logout=true"><?= $this->base->text('logout', 'button') ?></a>
+		        	<?php echo $this->base->text('user_inactive_note', 'paragraph') ?> <a href="<?php echo base_url() ?>/e/error_400?resend=true"><?php echo $this->base->text('resend_email', 'button') ?></a> <?php echo $this->base->text('or', 'label') ?> <a href="<?php echo base_url() ?>/e/error_400?logout=true"><?php echo $this->base->text('logout', 'button') ?></a>
 		        </p>
 	        </div>
 		</div>
 	</div>
-		<div class="hidden-area" style="position: fixed; bottom: 0; right: 0;">
+	<div class="hidden-area" style="position: fixed; bottom: 0; right: 0;">
 		<?php  
 		if(isset($_SESSION['msg'])){
 			$msg = json_decode($_SESSION['msg'], true);
